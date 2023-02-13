6dadcb1cd08edf5870ad7027d970baa70efb7939
moeenn <moeen.v8@gmail.com>
2023-02-13 10:44:57 +0500

    updated font: Cascadia Code -> JetBrains Mono

--- dmenu.c
+++ dmenu.c
@@ -18,7 +18,7 @@
                              * MAX(0, MIN((y)+(h),(r).y_org+(r).height) - MAX((y),(r).y_org)))
 #define MIN(a,b)              ((a) < (b) ? (a) : (b))
 #define MAX(a,b)              ((a) > (b) ? (a) : (b))
-#define DEFFONT "Cascadia Code:size=9" /* xft example: "Monospace-11" */
+#define DEFFONT "JetBrains Mono:size=9" /* xft example: "Monospace-11" */
 
 typedef struct Item Item;
 struct Item {
